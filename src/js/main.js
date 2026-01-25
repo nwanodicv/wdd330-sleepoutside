7f7d3107b1469290cf2cb7fb52d75883e2330f81
nwanodicv <victornwanodi@gmail.com>
2026-01-25 05:37:34 +0100

    update

--- src/js/main.js
+++ src/js/main.js
@@ -1,19 +1,3 @@
-// main.js
+import { loadHeaderFooter } from './utils.mjs';
 
-// Import ProductData class (handles fetching JSON)
-import ProductData from './productData.mjs';
-
-// Import ProductList class (handles rendering products)
-import ProductList from './productList.mjs';
-
-// Create data source using path to tents.json
-const dataSource = new ProductData('./json/tents.json');
-
-// Select the UL element where products will be rendered
-const listElement = document.querySelector('.product-list');
-
-// Create ProductList instance
-const productList = new ProductList('tents', dataSource, listElement);
-
-// Fetch products and render them to the UI
-productList.init();
+loadHeaderFooter();
---
+++ src/js/product-listing.js
@@ -0,0 +1,34 @@
+import { loadHeaderFooter, getParam } from './utils.mjs';
+import ProductData from './productData.mjs';
+import ProductList from './productList.mjs';
+
+loadHeaderFooter();
+
+const category = getParam('category');
+const dataSource = new ProductData();
+const element = document.querySelector('.product-list');
+const listing = new ProductList(category, dataSource, element);
+
+listing.init();
+
+
+//// main.js
+//
+//// Import ProductData class (handles fetching JSON)
+//import ProductData from './productData.mjs';
+//
+//// Import ProductList class (handles rendering products)
+//import ProductList from './productList.mjs';
+//
+//// Create data source using path to tents.json
+//const dataSource = new ProductData('./json/tents.json');
+//
+//// Select the UL element where products will be rendered
+//const listElement = document.querySelector('.product-list');
+//
+//// Create ProductList instance
+//const productList = new ProductList('tents', dataSource, listElement);
+//
+//// Fetch products and render them to the UI
+//productList.init();
+//
